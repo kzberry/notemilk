5b3635d36e3301a96e683465b3f17a16705c7a6f
kzberry <kberry24@adrian.edu>
2023-04-02 22:34:33 -0400

    Master

--- notemilk/apps.py
+++ notemilk/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class NotemilkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notemilk'
+
+class UsersConfig(AppConfig):
+    name = 'users'
